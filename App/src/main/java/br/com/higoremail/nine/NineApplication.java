0e2cbfc97202c0a8dba6b7a9395f4f521f3216c9
Higor do Nascimento Santos <higor.2120478300029@faeterj-rio.edu.br>
2023-12-20 11:17:47 -0300

    Add files via upload

---
+++ App/src/main/java/br/com/higoremail/nine/NineApplication.java
@@ -0,0 +1,13 @@
+package br.com.higoremail.nine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NineApplication.class, args);
+	}
+
+}
